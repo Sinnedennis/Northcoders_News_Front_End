b866c5e943d44ea6f2434e42cefd2ae3b16f4174
Dennis Foster <contact@DennisFoster.co.uk>
2017-12-28 21:41:18 +0000

    Adding deleteComment reducer tests

--- spec/helper.spec.js
+++ spec/helper.spec.js
@@ -4,7 +4,9 @@ const { orderArticles, getTime } = require('../src/utils');
 
 describe('#orderArticles', () => {
   it('should order a list based on prop "created_at"', () => {
+
     const d = new Date();
+
     const input = [
       { created_at: d.getTime() },
       { created_at: d.getTime() - 1000 },
@@ -20,6 +22,7 @@ describe('#orderArticles', () => {
     expect(newest[0].created_at).to.be.greaterThan(newest[1].created_at);
     expect(newest[2].created_at).to.be.lessThan(newest[1].created_at);
   });
+
 
   it('should order a list based on the prop "votes"', () => {
 
@@ -40,6 +43,7 @@ describe('#orderArticles', () => {
   });
 });
 
+
 describe('#getTime', () => {
     
   let pastTime = new Date();
@@ -50,11 +54,13 @@ describe('#getTime', () => {
     expect(getTime(pastTime - (61 * 10000))).to.not.equal(' less than a minute ago.');
   });
 
+
   it('should return " # minute(s) ago." when passed a difference of minutes', () => {
 
     expect(getTime(pastTime - (60 * 1000))).to.equal(' 1 minute ago.');
     expect(getTime(pastTime - (130 * 1000))).to.equal(' 2 minutes ago.');
   });
+
 
   it('as above with hours', () => {
 
@@ -62,11 +68,13 @@ describe('#getTime', () => {
     expect(getTime(pastTime - (7200 * 1000))).to.equal(' 2 hours ago.');
   });
 
+
   it('as above with days', () => {
 
     expect(getTime(pastTime - (86400 * 1000))).to.equal(' 1 day ago.');
     expect(getTime(pastTime - (188000 * 1000))).to.equal(' 2 days ago.');
   });
+
 
   it('as above with months', () => {
 
@@ -74,6 +82,7 @@ describe('#getTime', () => {
     expect(getTime(pastTime - (5184000 * 1000))).to.equal(' 2 months ago.');
   });
 
+  
   it('should return "over a year ago" when passed a difference of 1+ years', () => {
 
     expect(getTime(pastTime - (31556952 * 1000))).to.equal(' over a year ago.');
