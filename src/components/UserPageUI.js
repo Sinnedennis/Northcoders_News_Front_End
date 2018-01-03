43ece2c020f48b0dcf9809c53274e169e65f1edd
Dennis Foster <contact@DennisFoster.co.uk>
2018-01-03 18:12:28 +0000

    Adjusting html tag order

--- src/components/UserPageUI.js
+++ src/components/UserPageUI.js
@@ -4,15 +4,13 @@ import PT from 'prop-types';
 import { addDefaultAvatar } from '../utils';
 
 export default function UserPageUI({ user }) {
-  const { _id, avatar_url, name, username } = user;
+  const { avatar_url, name, username } = user;
 
   return (
     <div>
-      <p>I am a user page.</p>
-      <p>id: {_id}</p>
-      <img src={avatar_url} onError={addDefaultAvatar} alt="User Avatar" />
       <p>Name: {name}</p>
       <p>Username: {username}</p>
+      <img src={avatar_url} onError={addDefaultAvatar} alt="User Avatar" />
     </div>
   );
 }
