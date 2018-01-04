39b45fec736b506d866850e7d37ed589bb643c6f
Dennis Foster <contact@DennisFoster.co.uk>
2018-01-04 13:11:06 +0000

    Adding styling for user page

--- src/components/UserArticles.js
+++ src/components/UserArticles.js
@@ -3,11 +3,15 @@ import PT from 'prop-types';
 
 import ArticlePreview from './ArticlePreview';
 
+import '../styling/UserArticles.css';
+
 export default function UserArticles({ articles, userName }) {
 
   return (
     <div>
-      <p>Here are the articles by {userName}</p>
+      <div className="UserArticlesTag">
+        <p>Here are the articles by {userName}</p>
+      </div>
 
       {articles.map(articleObj => <ArticlePreview article={articleObj} key={articleObj._id} />)}
 
--- src/components/UserPageUI.js
+++ src/components/UserPageUI.js
@@ -3,14 +3,18 @@ import PT from 'prop-types';
 
 import { addDefaultAvatar } from '../utils';
 
+import '../styling/UserPage.css';
+
 export default function UserPageUI({ user }) {
   const { avatar_url, name, username } = user;
 
   return (
-    <div>
-      <p>Name: {name}</p>
-      <p>Username: {username}</p>
-      <img src={avatar_url} onError={addDefaultAvatar} alt="User Avatar" />
+    <div className="UserPage">
+      <h3 className="title">{username}</h3>
+      <h2 className="subtitle">{name}</h2>
+      <div className="UserImgContainer">
+        <img src={avatar_url} onError={addDefaultAvatar} alt="User Avatar" />
+      </div>
     </div>
   );
 }
